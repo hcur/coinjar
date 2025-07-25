a9a9e7fe92cb0087f50b4769a1872afcc71580d6
Hayden Curfman <hlc1@williams.edu>
2025-07-25 19:50:24 -0400

    finish basic api.tsx

--- web/src/api.tsx
+++ web/src/api.tsx
@@ -1,1 +1,56 @@
 import axios from 'axios';
+
+const api_url = 'http://localhost:3001';
+
+/* Account */
+
+export interface Account {
+    id: string;
+    name: string;
+    balance: number;
+}
+
+export interface Request_newAccount {
+    name: string;
+    balance: number;
+}
+
+/* Transaction */
+
+export interface Transaction {
+    id: string;
+    account_id: string;
+    source: string;
+    date: string;
+    amount: number;
+    note: string;
+}
+
+export interface Request_newTransaction {
+    account: string;
+    source: string;
+    date: string;
+    amount: number;
+    note: string;
+}
+
+const api = axios.create({
+    baseURL: api_url,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const accountsApi = {
+    getAll: () => api.get<Account[]>('/accounts'),
+    getById: (id: string) => api.get<Account>('/accounts/${id}'),
+    create: (data: Request_newTransaction) => api.post<Account>('/accounts', data),
+    delete: (id: string) => api.delete('/accounts/${id}'),
+};
+
+export const transactionsApi = {
+    create: (data: Request_newTransaction) => api.post<Transaction>('/transactions', data),
+    delete: (id: string) => api.delete('/transactions/${id}'),
+};
+
+export default api;
